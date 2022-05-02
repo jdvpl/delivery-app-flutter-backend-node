4328cb76e15a9c2ca6e9d0450772608dd4bf59e5
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-05-02 00:07:57 -0500

    users has roles

--- models/rol.js
+++ models/rol.js
@@ -4,14 +4,14 @@ const Rol={};
 
 Rol.create = (id_user,ud_rol)=>{
   const sql = 
-  `INSERT INTO
-    user_has_roles(
+  `
+  INSERT INTO user_has_roles(
       id_user,
       id_rol,
       created_at,
       updated_at
     )
-    VALUES($1,$2,$3,$4,)
+    VALUES($1,$2,$3,$4)
     `;
     return db.none(sql,[
       id_user,
